584d916ba42d250deea388597998c0396ddee75a
raphaelc484 <raphaelc484@hotmail.com>
2024-11-22 12:05:01 -0300

    feat: erro na preparação de ambiente para testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import path from "node:path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
